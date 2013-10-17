536404df91b516833508507200ed66100d2ca186
Tim Roediger <superdweebie@gmail.com>
2013-10-17 22:02:32 +1100

    make mystique play nice with dojo build layers

--- messages.js
+++ messages.js
@@ -1,17 +1,24 @@
 define([], function(){
     // module:
-    //		mystique/messages
+    //      mystique/messages
     //
     return {
         load: function(id, require, callback){
-            var jsonhttp = new XMLHttpRequest;
-            jsonhttp.onreadystatechange=function(){
-                if (jsonhttp.readyState==4 && jsonhttp.status==200) {
-                    callback(JSON.parse(jsonhttp.responseText));
+            if (require.micro) {
+                var jsonhttp = new XMLHttpRequest;
+                jsonhttp.onreadystatechange=function(){
+                    if (jsonhttp.readyState==4 && jsonhttp.status==200) {
+                        callback(JSON.parse(jsonhttp.responseText));
+                    }
                 }
+                jsonhttp.open("GET", require.baseUrl + "../mystique-common/translations/messages.json",true);
+                jsonhttp.send();
+            } else {
+                //this is added to allow caching of messages strings in a dojo layer
+                require(['dojo/text!mystique-common/translations/messages.json'], function(messages){
+                    callback(JSON.parse(messages));
+                })
             }
-            jsonhttp.open("GET", require.baseUrl + "../mystique-common/translations/messages.json",true);
-            jsonhttp.send();
         }
     };
 });
